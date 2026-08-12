fb991c8f9eff2236768a35b08e5db75dee9442b7
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2026-08-12 11:20:35 -0400

    1.0.5 bugfix

--- demo/src/main/java/user/demo/controller/controller.java
+++ demo/src/main/java/user/demo/controller/controller.java
@@ -3,6 +3,7 @@ package user.demo.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import user.demo.dto.UserDto;
